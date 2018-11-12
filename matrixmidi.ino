e61bf0bfa955ecbeaa0f45649f630d2f4a837dd8
J0J0 T <jojo@peek-a-boo.at>
2018-11-12 08:17:47 +0100

    test the matrix
    manually set high the outPin (column) to be tested in setup() lines 35-37
    and set row pin number in void loop() line 56.
    
    -> serial monitor shows all presses of column activated in setup()
    -> led only blinks when button in "correct" row is pressed (set in pinToTest)

--- matrixmidi.ino
+++ matrixmidi.ino
@@ -1,18 +1,16 @@
 #include <MIDI.h>
 #include <advancedSerial.h>
 #define USBserial Serial
-//#define MIDIserial Serial1
 #define VERBOSITY Level::vvv
 #define DEBUGGING on()
 //#define DEBUGGING off()
 
-//#MIDI_CREATE_INSTANCE(HardwareSerial, USBserial, MIDI); // port is selectable here
 
 const uint8_t ledPin = 13;      // LED pin on most Arduino
 
-byte outPins[] = {6,7,8}; // the rows
-byte inPins[] = {9,10,11,12}; // the columns
-byte rowChars[][4] = {
+const uint8_t outPins[] = {6,7,8}; // the rows
+const uint8_t inPins[] = {9,10,11,12}; // the columns
+const uint8_t rowChars[][3] = {
     {'1','2','3'},
     {'4','5','6'},
     {'7','8','9'},
@@ -23,21 +21,21 @@ uint8_t inVal0 = 0;
 uint8_t inVal1 = 0;
 uint8_t inVal2 = 0;
 uint8_t inVal3 = 0;
+uint8_t pinToTest = 0;      // initialize variable for row test blink
 
 void setup()
 {
     pinMode(ledPin, OUTPUT);
-    pinMode(outPins[0], OUTPUT);
-    pinMode(outPins[1], OUTPUT);
-    pinMode(outPins[2], OUTPUT);
-    pinMode(inPins[0], INPUT);
-    pinMode(inPins[1], INPUT);
-    pinMode(inPins[2], INPUT);
-    pinMode(inPins[3], INPUT);
-    //MIDI.begin(4);                      // Launch MIDI and listen to channel 4
-    digitalWrite(outPins[0], HIGH); // HIGH to check col 0
-    digitalWrite(outPins[1], LOW); // HIGH to check col 1
-    digitalWrite(outPins[2], LOW); // HIGH to check col 2
+    pinMode(outPins[0], OUTPUT); // our output line for col 0 (1,4,7,*)
+    pinMode(outPins[1], OUTPUT); // col 1 (2,5,8,0)
+    pinMode(outPins[2], OUTPUT); // col 2 (3,6,9,#)
+    pinMode(inPins[0], INPUT); // just for clarity, INPUT is the default mode anyway
+    pinMode(inPins[1], INPUT); // just for clarity, INPUT is the default mode anyway
+    pinMode(inPins[2], INPUT); // just for clarity, INPUT is the default mode anyway
+    pinMode(inPins[3], INPUT); // just for clarity, INPUT is the default mode anyway
+    digitalWrite(outPins[0], LOW); // set HIGH to check col 0
+    digitalWrite(outPins[1], LOW); // set HIGH to check col 1
+    digitalWrite(outPins[2], HIGH); // set HIGH to check col 2
     USBserial.begin(115200); // debugging settings start here
     aSerial.setPrinter(USBserial);
     aSerial.setFilter(VERBOSITY);
@@ -46,24 +44,19 @@ void setup()
 
 void loop()
 {
-    //MIDI.sendNoteOn(42, 127, 1);    // Send a Note (pitch 42, velo 127 on channel 1)
-    //MIDI.sendNoteOff(42, 0, 1);     // Stop the note
-    //if (digitalRead(inPins[0]) == HIGH) {
-    //    digitalWrite(ledPin, HIGH);
-
-        inVal0 = digitalRead(inPins[0]);
-        inVal1 = digitalRead(inPins[1]);
-        inVal2 = digitalRead(inPins[2]);
-        inVal3 = digitalRead(inPins[3]);
-        aSerial.vvv().p("inPin 0 value: ").pln(inVal0);
-        aSerial.vvv().p("inPin 1 value: ").pln(inVal1);
-        aSerial.vvv().p("inPin 2 value: ").pln(inVal2);
-        aSerial.vvv().p("inPin 3 value: ").pln(inVal3);
-        aSerial.vvv().pln();
-        delay(500);
-    //}
-    //else {
-    //    digitalWrite(ledPin, LOW);
-    //}
-    //delay(1000);
+    inVal0 = digitalRead(inPins[0]);
+    inVal1 = digitalRead(inPins[1]);
+    inVal2 = digitalRead(inPins[2]);
+    inVal3 = digitalRead(inPins[3]);
+    aSerial.vvv().p("inPin 0 value: ").pln(inVal0);
+    aSerial.vvv().p("inPin 1 value: ").pln(inVal1);
+    aSerial.vvv().p("inPin 2 value: ").pln(inVal2);
+    aSerial.vvv().p("inPin 3 value: ").pln(inVal3);
+    aSerial.vvv().pln();
+    pinToTest = digitalRead(inPins[3]); // which button row should be tested
+    if (pinToTest == HIGH) { // blink led if pinToTest on _row_ 0 is pressed)
+        digitalWrite(ledPin, HIGH);
+    }
+    delay(500);
+    digitalWrite(ledPin, LOW);
 }
